fdb7ef05c293d5eeb4c272e08cd084f757b6c823
rewish <rewish.org@gmail.com>
2009-09-28 16:44:28 +0900

    デフォルトの月移動コールバック関数でオプションを更新しないようにした。

--- jquery.calendar.js
+++ jquery.calendar.js
@@ -47,6 +47,9 @@
 	};
 
 	$c.setOption = function(option) {
+		if ($c.option && !option) {
+			return this;
+		}
 		$c.option = $.extend({
 			lang  : 'ja',
 			year  : $c.today.getFullYear(),
@@ -222,8 +225,8 @@
 			e.text(td.text());
 			td.text('').append(e).addClass('event');
 		},
-		move: function(elem, option) {
-			$c(elem, option);
+		move: function(elem) {
+			$c(elem);
 		}
 	};
 
