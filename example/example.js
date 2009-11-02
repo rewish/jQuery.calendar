dda49d0b4f04f90fa2de7f361e3543eb7d837a5c
rewish <rewish.org@gmail.com>
2009-11-02 18:09:04 +0900

    カレンダー導入例の変更。

--- example/example.js
+++ example/example.js
@@ -1,17 +1,18 @@
 jQuery(function($) {
 
-	$('#calendar').calendar({
+	$('#example').calendar({
+		// 初期表示する年
+		year: 2009,
+		// 初期表示する月
+		month: 10,
 		// イベントの設定
 		events: {
-			'2009-11-05': {
-				title: 'イベント1'
+			'2009-10-5': {
+				title: '今日はお休み'
 			},
-			'2009-11-15': {
-				title: 'イベント2',
+			'2009-10-15': {
+				title: 'Googleで検索する日',
 				url: 'http://google.co.jp/'
-			},
-			'2009-11-25': {
-				title: 'イベント3'
 			}
 		}
 	});
